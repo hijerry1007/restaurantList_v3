86e6b862800813090dd47f6fb953f2f9afcd3da9
Jerry Lai <jerrydavid123@hotmail.com>
2020-03-01 16:05:57 +0800

    Optimize new page

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -32,22 +32,38 @@ router.get('/:id', (req, res) => {
 //新增餐廳
 router.post('/', (req, res) => {
 
-  const restaurant = new Restaurant({
-    name: req.body.name,
-    name_en: req.body.name_en,
-    category: req.body.category,
-    image: req.body.image,
-    location: req.body.location,
-    phone: req.body.phone,
-    google_map: req.body.google_map,
-    rating: req.body.rating,
-    description: req.body.description,
-  })
+  if (req.body.name === '' || req.body.name_en === '' || req.body.category === '' || req.body.image === '' || req.body.location === '' || req.body.phone === '' || req.body.google_map === '' || req.body.rating === '' || req.body.description === '') {
+    let error = `所有格子都要輸入喔!`
+    return res.render('new', { error: error })
 
-  restaurant.save(err => {
-    if (err) return console.error(err)
-    return res.redirect('/')
-  })
+  }
+  else {
+    let checkdone
+    if (req.body.done === 'on') {
+      checkdone = true
+    }
+    else {
+      checkdone = false
+    }
+    const restaurant = new Restaurant({
+      name: req.body.name,
+      name_en: req.body.name_en,
+      category: req.body.category,
+      image: req.body.image,
+      location: req.body.location,
+      phone: req.body.phone,
+      google_map: req.body.google_map,
+      rating: req.body.rating,
+      description: req.body.description,
+      done: checkdone,
+    })
+
+    restaurant.save(err => {
+      if (err) return console.error(err)
+      return res.redirect('/')
+    })
+  }
+
 })
 
 // 修改餐廳詳細頁面
@@ -62,7 +78,7 @@ router.get('/:id/edit', (req, res) => {
 })
 
 // 修改餐廳
-router.put('/:id/edit', (req, res) => {
+router.put('/:id', (req, res) => {
   Restaurant.findById(req.params.id, (err, restaurant) => {
     if (err) return console.error(err)
 
