61c07824c51688ea1b05499c00f3495a62f2c64f
Jerry Lai <jerrydavid123@hotmail.com>
2020-03-05 22:13:23 +0800

    Install express-session

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -31,6 +32,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(session({
+  secret: "copy-cat",
+  resave: false,
+  saveUninitialized: true,
+}))
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
