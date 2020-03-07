cd05b82f749c7adb941f70f7a8d15a32464a50aa
Jerry Lai <jerrydavid123@hotmail.com>
2020-03-07 15:23:35 +0800

    Add connect flash and UI message

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
+const flash = require('connect-flash')
 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -46,14 +47,18 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 require('./config/passport')(passport)
+app.use(flash())
 
 app.use((req, res, next) => {
-
   res.locals.user = req.user
   //辨識使用者是否登入 => view 使用
   res.locals.isAuthenticated = req.isAuthenticated()
+
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
+
 
 app.use('/', require('./routes/home'))
 app.use('/restaurants', require('./routes/restaurant'))
--- routes/user.js
+++ routes/user.js
@@ -25,47 +25,67 @@ router.get('/register', (req, res) => {
 // 註冊檢查
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body
-  User.findOne({ email: email }).then(user => {
-    if (user) {
-      // email exist, can't send out
-      console.log('User already exists')
-      res.render('register', {                // 使用者已經註冊過
-        name,
-        email,
-        password,
-        password2
-      })
 
-    }
-    else {
-      // email doesn't exist, save and return to home page
-      const newUser = new User({
-        name,
-        email,
-        password
-      })
+  let errors = []
 
-      bcrypt.genSalt(10, (err, salt) =>
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err
-          newUser.password = hash
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: '所有欄位都是必填' })
+  }
+  if (password !== password2) {
+    errors.push({ message: '密碼輸入錯誤' })
+  }
 
-          newUser
-            .save()
-            .then(user => {
-              res.redirect('/')
-            })
-            .catch(err => console.log(err))
-
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    })
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        // email exist
+        errors.push({ message: '這個Email已經註冊過了' })
+        res.render('register', {
+          errors,                // 使用者已經註冊過
+          name,
+          email,
+          password,
+          password2
         })
-      )
-    }
-  })
+      }
+      else {
+        // email doesn't exist, save and return to home page
+        const newUser = new User({
+          name,
+          email,
+          password
+        })
+
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
+
+            newUser
+              .save()
+              .then(user => {
+                res.redirect('/')
+              })
+              .catch(err => console.log(err))
+          })
+        )
+      }
+    })
+  }
 })
 
 // 登出
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出')
   res.redirect('/users/login')
 })
 
